16aefe90d67a35804ed37dcd3332fcf8b6e8c1f3
Vishu <vishu@getperx.com>
2017-01-18 13:49:17 +0800

    v-tooltip.
    Removed template file.

--- addon/components/v-tooltip.js
+++ addon/components/v-tooltip.js
@@ -1,6 +1,85 @@
-import Ember from 'ember';
-import layout from '../templates/components/v-tooltip';
+// import Ember from 'ember';
+// // import layout from '../templates/components/v-tooltip';
 
-export default Ember.Component.extend({
-  layout
+// export default Ember.Component.extend({
+//   // layout
+// });
+
+//Not required template so removing layout dependency.
+
+import Ember from 'ember';
+import ClassPrefix from '../mixins/class-prefix';
+import TooltipEnabled from '../mixins/v-tooltip-enabled';
+import { throwTooltipError } from '../utils/error';
+
+/**
+ * @module
+ * @augments ember/Component
+ * @augments module:mixins/sl-tooltip-enabled
+ */
+export default Ember.Component.extend( ClassPrefix, TooltipEnabled, {
+
+    // -------------------------------------------------------------------------
+    // Dependencies
+
+    // -------------------------------------------------------------------------
+    // Attributes
+
+    /** @type {String} */
+    tagName: 'span',
+
+    // -------------------------------------------------------------------------
+    // Actions
+
+    // -------------------------------------------------------------------------
+    // Events
+
+    /**
+     * init event hook
+     *
+     * @returns {undefined}
+     */
+    init() {
+        this._super( ...arguments );
+        this.initialize();
+    },
+
+    // -------------------------------------------------------------------------
+    // Properties
+
+    /**
+     * Component class that will be prefixed with base component class
+     *
+     * @type {String}
+     */
+    componentClass: 'tooltip',
+
+    // -------------------------------------------------------------------------
+    // Observers
+
+    // -------------------------------------------------------------------------
+    // Methods
+
+    /**
+     * Check passed parameters on initialization
+     *
+     * @private
+     * @throws {sl-ember-components/utils/error/tooltip} Thrown if 'title' or 'popover' is invalid
+     * @returns {undefined}
+     */
+    initialize() {
+        if ( 'string' !== Ember.typeOf( this.get( 'title' ) ) ) {
+            throwTooltipError(
+                'enableTooltip() and enablePopover() expect the parameter "title" and for it to be a string'
+            );
+        }
+
+        if ( 'string' !== Ember.typeOf( this.get( 'popover' ) ) &&
+             'undefined' !== Ember.typeOf( this.get( 'popover' ) ) ) {
+            throwTooltipError(
+                'enablePopover() expects the parameter "popover" and for it to be a string'
+            );
+        }
+    }
+
 });
